2c1c65f90118e13b7ccf6f8a09dd6793f5eeffd5
ipyatigorets <igor.pyatigorets@exist.ua>
2024-02-09 05:34:02 +0200

    added docs, refactoring

--- pytest_openapi/plugin.py
+++ pytest_openapi/plugin.py
@@ -1,6 +1,4 @@
 import json
-import yaml
-import dotenv
 import pytest
 import requests
 import responses
@@ -9,28 +7,34 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 def pytest_addoption(parser):
     parser.addoption("--openapi-spec", action="store", help="Path to the OpenAPI spec file")
 
-def upload_fixture(file_path):
-    # with open(file_path, 'r') as f:
-    #     data = yaml.safe_load(f)
-    # return data
-    pass
+
+def upload_fixture(file_path=None):
+    return {
+        ('get', '/gcs/head/{bucket_name}/{focus_object}'): '/gcs/head/devlocalbucket/kiwi.png',
+        ('get', '/gcs/download/{bucket_name}/{source_file_name}/{destination_file_name}'): '/gcs/download/devlocalbucket/kiwi.png/kiwi.png',
+        ('put', '/gcs/upload/{bucket_name}/{source_file_name}/{destination_file_name}'): '/gcs/upload/devlocalbucket/kiwi.png/kiwi.png',
+        ('get', '/gcs/buckets/{project_id}'): '/gcs/buckets/titanium-arc-327213',
+        ('delete', '/gcs/{bucket_name}'): '/gcs/devlocalbucket',
+        ('put', '/gcs/{bucket_name}'): '/gcs/devlocalbucket',
+        ('get', '/aws/head/{bucket_name}/{focus_object}'): '/aws/head/devlocalbucket/kiwi.png',
+        ('get', '/aws/download/{bucket_name}/{source_file_name}/{destination_file_name}'): '/aws/download/devlocalbucket/kiwi.png/kiwi.png',
+        ('put', '/aws/upload/{bucket_name}/{source_file_name}/{destination_file_name}'): '/aws/upload/devlocalbucket/kiwi.png/kiwi.png',
+        ('get', '/aws/buckets/{project_id}'): '/aws/buckets/titanium-arc-327213',
+        ('delete', '/aws/{bucket_name}'): '/aws/devlocalbucket',
+        ('put', '/aws/{bucket_name}'): '/aws/devlocalbucket',
+    }
+
 
 @pytest.fixture
 def path_parameters(yaml_fixture):
-    # if yaml_fixture:
-    #     logger.debug(f"Upload yaml fixture: {yaml_fixture}")
-    #     fixture = upload_fixture(yaml_fixture)
-    # else:
-    #     logger.debug(f"Upload local .env fixture")
-    #     fixture = dotenv.dotenv_values(".env")
-    # return fixture
-    logger.debug(f"Upload local .env fixture")
-    fixture = dotenv.dotenv_values(".env")
+    logger.debug(f"Upload yaml fixture: {yaml_fixture}")
+    fixture = upload_fixture(yaml_fixture)
     return fixture
-    
+
 
 def pytest_generate_tests(metafunc):
     if 'openapi_test' in metafunc.fixturenames:
@@ -43,9 +47,16 @@ def pytest_generate_tests(metafunc):
                     tests.append((method, path))
         metafunc.parametrize("openapi_test", tests)
 
+
+def update_path(openapi_test: tuple):
+    method, path = openapi_test
+    path = upload_fixture().get(openapi_test, 'ATTENTION: Path not found!')
+    return method, path
+
+
 @responses.activate
-def test_openapi(openapi_test, path_parameters):
-    method, path, parameters = openapi_test
+def test_openapi(openapi_test: tuple):
+    method, path = update_path(openapi_test)
     responses.add(
         getattr(responses, method.upper()),
         f'http://localhost:8000{path}',
@@ -53,9 +64,5 @@ def test_openapi(openapi_test, path_parameters):
         status=200
     )
     url = f'http://localhost:8000{path}'
-    for parameter in parameters:
-        url = url.replace(f'{{{parameter}}}', path_parameters[parameter])
     response = getattr(requests, method)(url)
     assert response.status_code == 200
-
-
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytest-openapi',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     entry_points={
         'pytest11': [
@@ -13,6 +13,5 @@ setup(
         'pytest',
         'requests',
         'responses',
-        'dotenv',
     ],
 )
